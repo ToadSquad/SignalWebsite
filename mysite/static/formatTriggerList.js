fdf4a3478a923303f0c7a5d07de00f3ed8a860b2
ToadSquad <36092187+ToadSquad@users.noreply.github.com>
2022-01-05 18:10:37 -0500

    signals formatted

--- mysite/static/formatTriggerList.js
+++ mysite/static/formatTriggerList.js
@@ -9,13 +9,18 @@ window.addEventListener("load", function(){
     var string = ""
     for(element in elements){
         console.log(elements[element].innerText)
-        if(true){
+        try{
+        if(elements[element].innerText.length>3){
             string += `<div class="card">
                             <div class="text">${elements[element].innerText}</div>
                       </div>
             `
-            elements[element].innerHTML = ''
         }
+        }
+        catch{
+            console.log("undefined")
+        }
+        elements[element].innerHTML = ''
         
         
     }
